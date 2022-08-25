75c824d6885ac9a7efe924a3994aa4b45ed4b118
F4NT0 <gabriel.stundner@acad.pucrs.br>
2022-08-25 18:22:54 -0300

    #1 Criando um Schema

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
   * Links para como instalar eles
   * Como rodar o projeto no Intellij e VSCODE
   * Links para explicações do códigos
+
+## Versões dos programas utilizados
+
+* **Java 8** : [Como instalar o Java 8]()
+* **Postman** : [Download do postman mais recente]()
+* **Apache Maven 3.6.3** : [Como instalar o Maven]()
+* **PostgreSQL 12**: [Como instalar o PostgreSQL]()
+* 
--- src/main/java/com/ages/joinfut/Model/Template.java
+++ src/main/java/com/ages/joinfut/Model/Template.java
@@ -4,19 +4,13 @@ import com.ages.joinfut.DTO.TemplateDTO;
 import com.ages.joinfut.Enums.TemplateEnum;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "templates", schema = "examples")
 public class Template {
 
     @Id
