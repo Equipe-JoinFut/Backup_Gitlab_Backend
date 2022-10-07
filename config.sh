16a70b0dc6c565aaa9bc4855ddf6be2aec05aed3
F4NT0 <gabriel.stundner@acad.pucrs.br>
2022-10-07 14:11:32 -0300

    #15 Docker compose

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 mvn clean install -Dskiptests
 IF ERRORLEVEL 1 EXIT
-docker build -t joinfut/backend .
-docker tag joinfut/backend f4nt0/joinfut
-docker push f4nt0/joinfut
+sudo docker login
+sudo docker build -t joinfut/backend .
+sudo docker tag joinfut/backend f4nt0/joinfut
+sudo docker push f4nt0/joinfut
