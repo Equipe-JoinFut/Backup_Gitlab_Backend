7fa3cf91f30fdc8cedae38cad3678ae9996dacd2
ercosta1993 <edson-ti@outlook.com.br>
2022-10-21 21:05:21 -0300

    Correção das requisições do Clube

--- src/main/java/com/ages/joinfut/service/AtleteService.java
+++ src/main/java/com/ages/joinfut/service/AtleteService.java
@@ -196,6 +196,5 @@ public class AtleteService {
         return new Atlete(atleteDTO);
     }
 
-    public void calculateImc(Atlete atlete){ atlete.setAtleteImc(atlete.getAtleteWeight() / (Math.pow(atlete.getAtleteWeight(), 2))); }
-
+    public void calculateImc(Atlete atlete){ atlete.setAtleteImc(atlete.getAtleteWeight() / (Math.pow(atlete.getAtleteHeight(), 2))); }
 }
