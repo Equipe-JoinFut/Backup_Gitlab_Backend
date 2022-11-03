2498bebcfb4fd555d25cb8d6628b9b04510f0fea
Thiago Claas <t.chies@edu.pucrs.br>
2022-11-03 16:09:42 -0300

    #26 adiciona update() e convertList() ao VideoService

--- src/main/java/com/ages/joinfut/service/VideoService.java
+++ src/main/java/com/ages/joinfut/service/VideoService.java
@@ -2,15 +2,9 @@ package com.ages.joinfut.service;
 
 import com.ages.joinfut.config.mappers.AthleteMapper;
 import com.ages.joinfut.config.mappers.VideoMapper;
-import com.ages.joinfut.dto.AthleteDTO;
 import com.ages.joinfut.dto.VideoDTO;
-import com.ages.joinfut.model.Athlete;
-import com.ages.joinfut.model.AthleteClub;
 import com.ages.joinfut.model.Video;
-import com.ages.joinfut.repository.AdressRepository;
-import com.ages.joinfut.repository.AthleteClubRepository;
 import com.ages.joinfut.repository.AthleteRepository;
-import com.ages.joinfut.repository.ContactRepository;
 import com.ages.joinfut.repository.VideoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.transaction.Transactional;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -30,8 +23,8 @@ public class VideoService {
     @Autowired
     private VideoRepository videoRepository;
 
-    @Autowired
-    private AthleteRepository athleteRepository;
+    // @Autowired
+    // private AthleteRepository athleteRepository;
 
     private AthleteService athleteService= new AthleteService();
 
@@ -47,7 +40,7 @@ public class VideoService {
 
 
         if (video.getAthlete() != null) {
-            // video.getAthlete().setVideo(video); ta errado isso??
+            // video.getAthlete().setVideo(video); // ta errado isso??
             athleteService.save(AthleteMapper.MAPPER.AthleteToAthleteDTO(video.getAthlete()));
         }
 
@@ -66,9 +59,25 @@ public class VideoService {
         videoRepository.delete(video);
     }
 
+    @Transactional
+    public Video update(Long id, VideoDTO videoDTO, AthleteRepository athleteRepository) {
+        Video updated = VideoMapper.MAPPER.VideoDTOToVideo(videoDTO);
+
+        Video saved = videoRepository.findByidVideo(id);
+
+        if (updated.getAthlete() != null && !updated.getAthlete().equals(saved.getAthlete())) {
+            saved.setAthlete(updated.getAthlete());
+        }
+
+        if (updated.getDescription() != null && !updated.getDescription().equals(saved.getDescription())) {
+            saved.setDescription(updated.getDescription());
+        }
+
+        return saved;
+    }
 
 
-
-
-
+    public List<VideoDTO> convertList(List<Video> videos) {
+        return videos.stream().map(video -> VideoMapper.MAPPER.VideoToVideoDTO(video)).collect(Collectors.toList());
+    }
 }
