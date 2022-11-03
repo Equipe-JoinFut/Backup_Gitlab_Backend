2eafd746dbd824c0b88fbf28b80a3f66c3ee75ed
Vittoria <vittoria.presa02@edu.pucrs.br>
2022-11-02 21:51:36 -0300

    Filtros do atleta (menos o adress.state)

--- src/main/java/com/ages/joinfut/controller/AthleteController.java
+++ src/main/java/com/ages/joinfut/controller/AthleteController.java
@@ -1,23 +1,23 @@
 package com.ages.joinfut.controller;
 
+import com.ages.joinfut.Enum.DominantLeg;
+import com.ages.joinfut.Enum.Position;
+import com.ages.joinfut.Enum.State;
 import com.ages.joinfut.config.mappers.AthleteMapper;
+import com.ages.joinfut.dto.AdressDTO;
 import com.ages.joinfut.dto.AthleteDTO;
+import com.ages.joinfut.model.Adress;
 import com.ages.joinfut.model.Athlete;
 import com.ages.joinfut.repository.AthleteRepository;
+import com.ages.joinfut.repository.AdressRepository;
 import com.ages.joinfut.service.AthleteService;
+import com.ages.joinfut.service.AdressService;
 import io.swagger.annotations.ApiModelProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
@@ -93,4 +93,84 @@ public class AthleteController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    @GetMapping(value = URL_PLURAL, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiModelProperty("Busca em lista as alturas dos atletas")
+    @Transactional
+    public ResponseEntity<List<AthleteDTO>> readAthleteByathleteHeight(@RequestParam(value = "athleteHeight", required = false) Double athleteHeight) {
+        if (athleteHeight != null) {
+            List<Athlete> athletes = athleteRepository.findByathleteHeight(athleteHeight);
+            List<AthleteDTO> athleteDTO = athleteService.convertList(athletes);
+            return new ResponseEntity<>(athleteDTO, HttpStatus.OK);
+        } else {
+            List<Athlete> athletes = athleteRepository.findAll();
+            List<AthleteDTO> athleteDTO = athleteService.convertList(athletes);
+            return new ResponseEntity<>(athleteDTO, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping(value = URL_PLURAL, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiModelProperty("Busca em lista as idades dos atletas")
+    @Transactional
+    public ResponseEntity<List<AthleteDTO>> readAthleteByage(@RequestParam(value = "age", required = false) Integer age) {
+        if (age != null) {
+            List<Athlete> athletes = athleteRepository.findByage(age);
+            List<AthleteDTO> athleteDTO = athleteService.convertList(athletes);
+            return new ResponseEntity<>(athleteDTO, HttpStatus.OK);
+        } else {
+            List<Athlete> athletes = athleteRepository.findAll();
+            List<AthleteDTO> athleteDTO = athleteService.convertList(athletes);
+            return new ResponseEntity<>(athleteDTO, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping(value = URL_PLURAL, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiModelProperty("Busca em lista os pesos dos atletas")
+    @Transactional
+    public ResponseEntity<List<AthleteDTO>> readAthelteByathleteWeight(@RequestParam(value = "athleteWeight", required = false) Double athleteWeight) {
+        if (athleteWeight != null) {
+            List<Athlete> athletes = athleteRepository.findByathleteWeight(athleteWeight);
+            List<AthleteDTO> athleteDTO = athleteService.convertList(athletes);
+            return new ResponseEntity<>(athleteDTO, HttpStatus.OK);
+        } else {
+            List<Athlete> athletes = athleteRepository.findAll();
+            List<AthleteDTO> athleteDTO = athleteService.convertList(athletes);
+            return new ResponseEntity<>(athleteDTO, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping(value = URL_PLURAL, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiModelProperty("Busca em lista as pernas dominantes dos atletas")
+    @Transactional
+    public ResponseEntity<List<AthleteDTO>> readAthleteBydominantLeg(@RequestParam(value = "dominantLeg", required = false) DominantLeg dominantLeg) {
+        if (dominantLeg != null) {
+            List<Athlete> athletes = athleteRepository.findBydominantLeg(dominantLeg);
+            List<AthleteDTO> athleteDTO = athleteService.convertList(athletes);
+            return new ResponseEntity<>(athleteDTO, HttpStatus.OK);
+        } else {
+            List<Athlete> athletes = athleteRepository.findAll();
+            List<AthleteDTO> athleteDTO = athleteService.convertList(athletes);
+            return new ResponseEntity<>(athleteDTO, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping(value = URL_PLURAL, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiModelProperty("Busca em lista as posições dos atletas")
+    @Transactional
+    public ResponseEntity<List<AthleteDTO>> readAthleteByposition(@RequestParam(value = "position", required = false) Position position) {
+        if (position != null) {
+            List<Athlete> athletes = athleteRepository.findByposition(position);
+            List<AthleteDTO> athleteDTO = athleteService.convertList(athletes);
+            return new ResponseEntity<>(athleteDTO, HttpStatus.OK);
+        } else {
+            List<Athlete> athletes = athleteRepository.findAll();
+            List<AthleteDTO> athleteDTO = athleteService.convertList(athletes);
+            return new ResponseEntity<>(athleteDTO, HttpStatus.OK);
+        }
+    }
+
+    //Adress State
+
 }
+
+
--- src/main/java/com/ages/joinfut/repository/AthleteRepository.java
+++ src/main/java/com/ages/joinfut/repository/AthleteRepository.java
@@ -1,10 +1,29 @@
 package com.ages.joinfut.repository;
 
+import com.ages.joinfut.Enum.DominantLeg;
+import com.ages.joinfut.Enum.Position;
+import com.ages.joinfut.Enum.State;
+import com.ages.joinfut.model.Adress;
 import com.ages.joinfut.model.Athlete;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AthleteRepository extends JpaRepository<Athlete, Long> {
     Athlete findByidAthlete(Long id);
+
+    List<Athlete> findByathleteHeight(Double athleteHeight);
+
+    List<Athlete> findByage(Integer age);
+
+    List<Athlete> findByathleteWeight(Double athleteWeight);
+
+    List<Athlete> findBydominantLeg(DominantLeg dominantLeg);
+
+    List<Athlete> findByposition(Position position);
+
+    List<Adress> findBystate(State state);
+
 }
