88c6dd143161ad552bc814face58ec51d187e619
Thiago Claas <t.chies@edu.pucrs.br>
2022-11-02 21:50:31 -0300

    #26  adiciona save() do VideoService, olha a linha 50 Fanto

---
+++ src/main/java/com/ages/joinfut/service/VideoService.java
@@ -0,0 +1,59 @@
+package com.ages.joinfut.service;
+
+import com.ages.joinfut.config.mappers.AthleteMapper;
+import com.ages.joinfut.config.mappers.VideoMapper;
+import com.ages.joinfut.dto.AthleteDTO;
+import com.ages.joinfut.dto.VideoDTO;
+import com.ages.joinfut.model.Athlete;
+import com.ages.joinfut.model.AthleteClub;
+import com.ages.joinfut.model.Video;
+import com.ages.joinfut.repository.AdressRepository;
+import com.ages.joinfut.repository.AthleteClubRepository;
+import com.ages.joinfut.repository.AthleteRepository;
+import com.ages.joinfut.repository.ContactRepository;
+import com.ages.joinfut.repository.VideoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+@Service
+public class VideoService {
+
+    @Autowired
+    private VideoRepository videoRepository;
+
+    @Autowired
+    private AthleteRepository athleteRepository;
+
+    private AthleteService athleteService= new AthleteService();
+
+
+    public VideoService() {}
+
+    @Transactional
+    public Video save(VideoDTO videoDTO) {
+        
+        Video video = VideoMapper.MAPPER.VideoDTOToVideo(videoDTO);
+
+        videoRepository.save(video);
+
+
+        if (video.getAthlete() != null) {
+            // video.getAthlete().setVideo(video); ta errado isso??
+            athleteService.save(AthleteMapper.MAPPER.AthleteToAthleteDTO(video.getAthlete()));
+        }
+
+        return video;
+    }
+
+
+    
+}
