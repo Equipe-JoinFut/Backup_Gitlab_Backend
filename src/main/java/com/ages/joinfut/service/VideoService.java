c1153fca34a45d77af6d8bb8a467da612a3053f7
Thiago Claas <t.chies@edu.pucrs.br>
2022-11-04 18:06:43 -0300

    #26 corrige o save() do VideoService

--- src/main/java/com/ages/joinfut/service/VideoService.java
+++ src/main/java/com/ages/joinfut/service/VideoService.java
@@ -22,28 +22,15 @@ public class VideoService {
     @Autowired
     private VideoRepository videoRepository;
 
-    // @Autowired
-    // private AthleteRepository athleteRepository;
 
-    private AthleteService athleteService= new AthleteService();
+    private VideoService videoService = new VideoService();
 
 
     public VideoService() {}
 
     @Transactional
-    public Video save(VideoDTO videoDTO) {
-        
-        Video video = VideoMapper.MAPPER.VideoDTOToVideo(videoDTO);
-
+    public void save(Video video, VideoRepository videoRepository) {
         videoRepository.save(video);
-
-
-        if (video.getAthlete() != null) {
-            // video.getAthlete().setVideo(video); // ta errado isso??
-            athleteService.save(AthleteMapper.MAPPER.AthleteToAthleteDTO(video.getAthlete()));
-        }
-
-        return video;
     }
 
     @Transactional
